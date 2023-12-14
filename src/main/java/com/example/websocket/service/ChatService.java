88ba81200167c428939aba9b1bcb01ba6f6822ef
J1W0N-1209 <nabook754@naver.com>
2023-12-14 17:10:49 +0900

    ✨ :: ChatService

--- src/main/java/com/example/websocket/service/ChatService.java
+++ src/main/java/com/example/websocket/service/ChatService.java
@@ -1,4 +1,54 @@
 package com.example.websocket.service;
 
+import com.example.websocket.dto.ChatRoom;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.*;
+
+@Slf4j
+@Data
+@Service
 public class ChatService {
+    private final ObjectMapper mapper;
+    private Map<String, ChatRoom> chatRooms;
+
+    @PostConstruct // bean lifeCycle에서 오직 한 번만 수행된다. (여러 번 초기화 방지)
+    private void init() {
+        chatRooms = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoom> findAllRoom() {
+        return new ArrayList<>(chatRooms.values());
+    }
+
+    public ChatRoom findRoomById(String roomId) {
+        return chatRooms.get(roomId);
+    }
+
+    public ChatRoom createRoom(String name) {
+        String roomID = UUID.randomUUID().toString();
+
+        ChatRoom room = ChatRoom.builder()
+                .roomId(roomID)
+                .name(name)
+                .build();
+
+        chatRooms.put(roomID,room);
+        return room;
+    }
+
+    public <T> void sendMessage(WebSocketSession session, T message) {
+        try {
+            session.sendMessage(new TextMessage(mapper.writeValueAsString(message)));
+        } catch (IOException e) {
+            log.error(e.getMessage(),e);
+        }
+    }
 }
